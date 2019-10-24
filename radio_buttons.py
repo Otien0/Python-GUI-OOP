aa6b57fd2177bfd2a07d3b4a08c35fbdd2ef803a
mourice-oduor <otienomaurice364@gmail.com>
2019-10-24 18:06:48 -0400

    creating a radio button

---
+++ radio_buttons.py
@@ -0,0 +1,13 @@
+from tkinter import *
+root = Tk()
+root.title('radio buttons')
+
+Radiobutton(root,text='radio1',padx=40).pack(anchor=W)
+Radiobutton(root,text='radio2',padx=40).pack(anchor=W)
+Radiobutton(root,text='radio3',padx=40).pack(anchor=W)
+Radiobutton(root,text='radio4',padx=40).pack(anchor=W)
+
+
+
+
+root.mainloop()
